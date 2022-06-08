121ccb3a7bbb30ab1fcc4cbb0d455afb08ce941c
Moshood-wale <moshood815@gmail.com>
2022-06-08 14:00:02 +0100

    fixed heroku issues

--- config/settings/settings.py
+++ config/settings/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from decouple import config
 import dj_database_url
 from django.core.management.utils import get_random_secret_key
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,3 +146,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
